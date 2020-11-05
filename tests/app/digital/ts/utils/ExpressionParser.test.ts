08aac626b53f23d5400955f4e99b7a8162b39c9e
Trevor Crystal <TGCrystal@users.noreply.github.com>
2020-11-05 00:34:55 -0500

    another failed test try

--- tests/app/digital/ts/utils/ExpressionParser.test.ts
+++ tests/app/digital/ts/utils/ExpressionParser.test.ts
@@ -12,28 +12,24 @@ import {IOObject} from "core/models/IOObject";
 
 
 describe("Trevor Test", () => {
+
+      const designer = new DigitalCircuitDesigner(0)
+      const a = new Switch(), b = new Switch(), o = new LED(), and_gate = new ANDGate();
+      const w1 = new DigitalWire(a.getOutputPort(0), and_gate.getInputPort(0))
+      const w2 = new DigitalWire(b.getOutputPort(0), and_gate.getInputPort(1))
+      const w3 = new DigitalWire(and_gate.getOutputPort(0), o.getInputPort(0))
+
+      let objectSet = new DigitalObjectSet([a, b, o, and_gate, w1, w2, w3])
+      designer.addGroup(objectSet)
+
     test("Test1", () => {
-        const designer = new DigitalCircuitDesigner(0);
-
-        const a = new Switch(), b = new Switch(), o = new LED(), and_gate = new ANDGate();
-        // const a = new Switch();
-        const w1 = new DigitalWire(a.getOutputPort(0), and_gate.getInputPort(0))
-        const w2 = new DigitalWire(b.getOutputPort(0), and_gate.getInputPort(1))
-        const w3 = new DigitalWire(and_gate.getOutputPort(0), o.getInputPort(0))
-        let set = new DigitalObjectSet([a, b, o, and_gate, w1, w2, w3]);
-
-        designer.addGroup(set)
-
         a.activate(true);
-
         expect(o.isOn()).toBe(false);
 
         b.activate(true);
-
         expect(o.isOn()).toBe(true);
 
         a.activate(false);
-
         expect(o.isOn()).toBe(false);
     });
 });
