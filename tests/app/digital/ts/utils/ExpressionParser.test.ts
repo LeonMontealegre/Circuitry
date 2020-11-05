7cf0dc1142ebd81e44ca5c1431542e695dc4b317
Trevor Crystal <TGCrystal@users.noreply.github.com>
2020-11-04 23:13:11 -0500

    Attempt at understanding how to format tests

---
+++ tests/app/digital/ts/utils/ExpressionParser.test.ts
@@ -0,0 +1,39 @@
+import "jest";
+
+
+import {DigitalCircuitDesigner} from "digital/models/DigitalCircuitDesigner";
+import {Switch}              from "digital/models/ioobjects/inputs/Switch";
+import {ANDGate}         from "digital/models/ioobjects/gates/ANDGate";
+import {ORGate}            from "digital/models/ioobjects/gates/ORGate";
+import {LED}                 from "digital/models/ioobjects/outputs/LED";
+import {DigitalWire} from "digital/models/DigitalWire";
+import {DigitalObjectSet} from "digital/utils/ComponentUtils";
+import {IOObject} from "core/models/IOObject";
+
+
+describe("Trevor Test", () => {
+    test("Test1", () => {
+        const designer = new DigitalCircuitDesigner(0);
+
+        const a = new Switch(), b = new Switch(), o = new LED(), and_gate = new ANDGate();
+        // const a = new Switch();
+        const w1 = new DigitalWire(a.getOutputPort(0), and_gate.getInputPort(0))
+        const w2 = new DigitalWire(b.getOutputPort(0), and_gate.getInputPort(1))
+        const w3 = new DigitalWire(and_gate.getOutputPort(0), o.getInputPort(0))
+        let set = new DigitalObjectSet([a, b, o, and_gate, w1, w2, w3]);
+
+        designer.addGroup(set)
+
+        a.activate(true);
+
+        expect(o.isOn()).toBe(false);
+
+        b.activate(true);
+
+        expect(o.isOn()).toBe(true);
+
+        a.activate(false);
+
+        expect(o.isOn()).toBe(false);
+    });
+});
