e747a8870fb416447f345c8be44b60a8a9491119
Trevor Crystal <TGCrystal@users.noreply.github.com>
2020-11-05 23:12:45 -0500

    Added a couple more minutes

--- tests/app/digital/ts/utils/ExpressionParser.test.ts
+++ tests/app/digital/ts/utils/ExpressionParser.test.ts
@@ -302,6 +302,31 @@ describe("Expression Parser", () => {
             designer.addGroup(objectSet);
 
             test("Initial State", () => {
+                expect(o.isOn()).toBe(true);
+            });
+            test("Input on", () => {
+                a.activate(true);
+
+                expect(o.isOn()).toBe(false);
+            });
+            test("Input off", () => {
+                a.activate(false);
+
+                expect(o.isOn()).toBe(true);
+            });
+        });
+
+        describe("Parse: '!!a'", () => {
+            const designer = new DigitalCircuitDesigner(0);
+            const a = new Switch(), o = new LED();
+            const inputMap = new Map([
+                ["a", a]
+            ]);
+
+            const objectSet = ExpressionToCircuit(inputMap, "!!a", o);
+            designer.addGroup(objectSet);
+
+            test("Initial State", () => {
                 expect(o.isOn()).toBe(false);
             });
             test("Input on", () => {
@@ -810,6 +835,65 @@ describe("Expression Parser", () => {
             ]);
 
             const objectSet = ExpressionToCircuit(inputMap, "a&b|c", o);
+            designer.addGroup(objectSet);
+
+            test("Initial State", () => {
+                expect(o.isOn()).toBe(false);
+            });
+            test("Input a on", () => {
+                a.activate(true);
+
+                expect(o.isOn()).toBe(false);
+            });
+            test("Input a,b on", () => {
+                b.activate(true);
+
+                expect(o.isOn()).toBe(true);
+            });
+            test("Input a,c on", () => {
+                b.activate(false);
+                c.activate(true);
+
+                expect(o.isOn()).toBe(false);
+            });
+            test("Input a,b,c on", () => {
+                b.activate(true);
+
+                expect(o.isOn()).toBe(true);
+            });
+            test("Input b,c on", () => {
+                a.activate(false);
+
+                expect(o.isOn()).toBe(true);
+            });
+            test("Input c on", () => {
+                b.activate(false);
+
+                expect(o.isOn()).toBe(true);
+            });
+            test("Input b on", () => {
+                b.activate(true);
+                c.activate(false);
+
+                expect(o.isOn()).toBe(false);
+            });
+            test("Inputs off", () => {
+                b.activate(false);
+
+                expect(o.isOn()).toBe(false);
+            });
+        });
+
+        describe("Parse: 'c|a&b'", () => {
+            const designer = new DigitalCircuitDesigner(0);
+            const a = new Switch(), b = new Switch(), c = new Switch(), o = new LED();
+            const inputMap = new Map([
+                ["a", a],
+                ["b", b],
+                ["c", c]
+            ]);
+
+            const objectSet = ExpressionToCircuit(inputMap, "c|a&b", o);
             designer.addGroup(objectSet);
 
             test("Initial State", () => {
