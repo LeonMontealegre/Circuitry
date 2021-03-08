7f88df7f232a9341781433f0e9f1b02ec3b03d06
Leon Montealegre <leonm99@gmail.com>
2021-03-07 22:39:54 -0500

    Added initial docusaurus config

---
+++ docs/docusaurus.config.js
@@ -0,0 +1,103 @@
+module.exports = {
+  title: "OpenCircuits",
+  tagline: "The free, online, circuit designer",
+  url: "https://your-docusaurus-test-site.com",
+  baseUrl: "/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.ico",
+  organizationName: "OpenCircuits", // Usually your GitHub org/user name.
+  projectName: "OpenCircuits", // Usually your repo name.
+  themeConfig: {
+    navbar: {
+      title: "OpenCircuits",
+      logo: {
+        alt: "OpenCircuits Logo",
+        src: "img/icon.svg",
+      },
+      items: [
+        {
+          label: "Docs",
+          type: "doc",
+          docId: "test",
+          position: "left"
+        },
+        {
+          label: "API",
+          type: "doc",
+          docId: "test",
+          position: "left"
+        },
+        {
+          label: "JSDocs",
+          type: "doc",
+          docId: "js/app/core/utils/math/Vector",
+          position: "left"
+        },
+        {
+          label: "Other",
+          type: "doc",
+          docId: "test",
+          position: "left"
+        },
+        {
+          href: "https://github.com/OpenCircuits/OpenCircuits",
+          label: "GitHub",
+          position: "right",
+        },
+      ],
+    },
+    footer: {
+      style: "dark",
+      links: [
+        {
+          title: "Docs",
+          items: [
+            {
+              label: "Style Guide",
+              to: "docs/",
+            },
+            {
+              label: "Second Doc",
+              to: "docs/doc2/",
+            },
+          ],
+        },
+        {
+          title: "Community",
+          items: [
+            {
+              label: "Discord",
+              href: "https://discordapp.com/invite/bCV2tYFer9",
+            }
+          ],
+        },
+        {
+          title: "More",
+          items: [
+            {
+              label: "GitHub",
+              href: "https://github.com/OpenCircuits/OpenCircuits",
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} OpenCircuits`,
+    },
+  },
+  presets: [
+    [
+      "@docusaurus/preset-classic",
+      {
+        docs: {
+          routeBasePath: "/",
+          sidebarPath: require.resolve("./sidebars.js"),
+          editUrl: "https://github.com/OpenCircuits/OpenCircuits/edit/master/docs/",
+        },
+        theme: {
+          customCss: require.resolve("./src/css/custom.css"),
+        },
+      },
+    ],
+  ],
+};
---
+++ docs/sidebars.js
@@ -0,0 +1,16 @@
+module.exports = {
+  mainSidebar: {
+    "OpenCircuits": ["test"],
+    "Getting Started": [],
+    "Guides": []
+  },
+  apiSidebar: {
+    "API": []
+  },
+  jsDocSidebar: {
+    "JSDocs": ["js/app/core/utils/math/Vector"]
+  },
+  otherSidebar: {
+    "Other": []
+  }
+};
